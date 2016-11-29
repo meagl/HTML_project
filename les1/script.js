b543401cfb8d0c972614668e0ab7720b5341fdfd
meagl <boxxxw@mail.ru>
2016-11-29 16:39:06 +0300

    Add files via upload

--- les1/script.js
+++ les1/script.js
@@ -1,12 +1,12 @@
 
 function show_pic(){
-//alert('hi');
 var c1_elem = document.getElementById("c1");
 var c2_elem = document.getElementById("c2");
 var c3_elem = document.getElementById("c3");
 c1_elem.style.display='none';
 c2_elem.style.display = 'block';
 c3_elem.style.display = 'none';
+
 }
 
 function show_text(){
@@ -16,6 +16,7 @@ var c3_elem = document.getElementById("c3");
 c1_elem.style.display='block';
 c2_elem.style.display = 'none';
 c3_elem.style.display = 'none';
+
 }
 
 function show_game() {
@@ -25,7 +26,9 @@ function show_game() {
     c1_elem.style.display = 'none';
     c2_elem.style.display = 'none';
     c3_elem.style.display = 'block';
+	
 }
+
 
 var res="21332"; 
 function check_me()
